823b5036179154adbeea513353ce95bf06f1dad1
Tomáš grofek <grofy1@gmail.com>
2019-11-19 19:49:05 +0100

    added search button

--- src/components/AppliedGamesList/index.jsx
+++ src/components/AppliedGamesList/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
 import AppliedGame from "./AppliedGame";
+import Button from "@material-ui/core/Button";
+import {Search} from "@material-ui/icons";
+import {bindActionCreators} from "redux";
+import {push} from "react-router-redux";
+import {connect} from "react-redux";
+import style from './AppliedGamesList.module.css'
 
 
 class AppliedGamesList extends React.Component {
@@ -11,10 +17,21 @@ class AppliedGamesList extends React.Component {
         )
     }
 
+    searchEvents = () => {
+        this.props.push('/');
+    };
+
     renderGames = () => {
         if (this.props.applications.length < 1) {
             return (
-                <h2>Nemáte žádné přihlášené akce</h2>
+                <div className={style.wrapper}>
+                    <h2>Nemáte žádné přihlášené akce</h2>
+                    <Button startIcon={<Search/>} variant="contained" color="primary"
+                            onClick={this.searchEvents}>
+                        Vyhledat akce
+                    </Button>
+                </div>
+
             )
         }
 
@@ -35,4 +52,10 @@ class AppliedGamesList extends React.Component {
 }
 
 
-export default AppliedGamesList;
+function mapDispatchToProps(dispatch) {
+    return {
+        push: bindActionCreators(push, dispatch),
+    }
+}
+
+export default connect(null, mapDispatchToProps)(AppliedGamesList);
